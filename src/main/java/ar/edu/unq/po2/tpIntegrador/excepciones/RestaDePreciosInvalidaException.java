deea3ff99ae55e6387481d38d5cc75c608b125af
salvanescki <salvanescki.unq@gmail.com>
2024-11-06 22:17:07 -0300

    Agrego excepciones para Precio

---
+++ src/main/java/ar/edu/unq/po2/tpIntegrador/excepciones/RestaDePreciosInvalidaException.java
@@ -0,0 +1,7 @@
+package ar.edu.unq.po2.tpIntegrador.excepciones;
+
+public class RestaDePreciosInvalidaException extends RuntimeException {
+    public RestaDePreciosInvalidaException(String message) {
+        super(message);
+    }
+}
