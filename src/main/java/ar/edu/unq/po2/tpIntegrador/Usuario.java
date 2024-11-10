a03bfbb2c3e56ae4f376cac4f5bb640ae9d7fd7f
salvanescki <salvanescki.unq@gmail.com>
2024-11-10 13:47:22 -0300

    Mejora de mockeo y añado a Usuario mensajes

--- src/main/java/ar/edu/unq/po2/tpIntegrador/Usuario.java
+++ src/main/java/ar/edu/unq/po2/tpIntegrador/Usuario.java
@@ -1,4 +1,65 @@
 package ar.edu.unq.po2.tpIntegrador;
 
-public class Usuario {
+import java.util.List;
+
+public class Usuario implements Propietario, Inquilino{
+    @Override
+    public List<Reserva> getReservas() {
+        return List.of();
+    }
+
+    @Override
+    public List<Reserva> getReservasFuturas() {
+        return List.of();
+    }
+
+    @Override
+    public List<Reserva> getReservasEnCiudad(String ciudad) {
+        return List.of();
+    }
+
+    @Override
+    public List<String> getCiudadesConReservas() {
+        return List.of();
+    }
+
+    @Override
+    public int getCantidadDeVecesQueAlquilo() {
+        return 0;
+    }
+
+    @Override
+    public int cantidadDeVecesQuePublicoInmuebles() {
+        return 0;
+    }
+
+    @Override
+    public List<Publicacion> getInmueblesEnAlquiler() {
+        return List.of();
+    }
+
+    @Override
+    public void puntuar(Usuario usuario, int puntaje, String comentario, Categoria categoria) {
+
+    }
+
+    @Override
+    public double getPuntajePromedioEnCategoria(Categoria categoria) {
+        return 0;
+    }
+
+    @Override
+    public double getPuntajePromedioTotal() {
+        return 0;
+    }
+
+    @Override
+    public List<String> getComentariosDeInquilinosPrevios() {
+        return List.of();
+    }
+
+    @Override
+    public int getPuntajeDeUsuarioEnCategoria(Usuario usuario, Categoria categoria) {
+        return 0;
+    }
 }
