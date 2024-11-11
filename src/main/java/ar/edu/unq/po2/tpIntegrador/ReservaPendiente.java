af5a8558c95c41b278226ca7010067c4f9e862d5
Abril <arodriguezfontana@gmail.com>
2024-11-11 20:25:33 -0300

    WIP Reserva Tests

---
+++ src/main/java/ar/edu/unq/po2/tpIntegrador/ReservaPendiente.java
@@ -0,0 +1,27 @@
+package ar.edu.unq.po2.tpIntegrador;
+
+public class ReservaPendiente implements EstadoReserva {
+
+    @Override
+    public void aprobarReserva() {
+    }
+
+    @Override
+    public void cancelarReserva() {
+    }
+
+    @Override
+    public boolean estaAprobada() {
+        return false;
+    }
+
+    @Override
+    public boolean estaPendiente() {
+        return false;
+    }
+
+    @Override
+    public boolean fueCancelada() {
+        return false;
+    }
+}
