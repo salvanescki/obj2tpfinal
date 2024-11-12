8146c79dfaaae912fabdad142e6b5771d5c4ace7
Cristian <cristian.adrian.f.r@gmail.com>
2024-11-12 14:02:31 -0300

    Se realizaron los test de la entidad Servicio

---
+++ src/test/java/ar/edu/unq/po2/tpIntegrador/ServicioTest.java
@@ -0,0 +1,28 @@
+package ar.edu.unq.po2.tpIntegrador;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ServicioTest {
+
+    @Test
+    void testConstructor() {
+
+        String nombreServicio = "Internet";
+
+        Servicio servicio = new Servicio(nombreServicio);
+
+        assertEquals(nombreServicio, servicio.getServicio());
+    }
+
+    @Test
+    void testSetServicio() {
+
+        Servicio servicio = new Servicio("Telefonía");
+        String nuevoServicio = "Cable TV";
+
+        servicio.setServicio(nuevoServicio);
+
+        assertEquals(nuevoServicio, servicio.getServicio());
+    }
+}
