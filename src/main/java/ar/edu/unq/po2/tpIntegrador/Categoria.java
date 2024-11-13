ae12284a67363c3fcdf9d58eb7fdedccca8dfa30
Cristian <cristian.adrian.f.r@gmail.com>
2024-11-12 23:23:27 -0300

    Hice la clase Categoria

--- src/main/java/ar/edu/unq/po2/tpIntegrador/Categoria.java
+++ src/main/java/ar/edu/unq/po2/tpIntegrador/Categoria.java
@@ -1,7 +1,18 @@
 package ar.edu.unq.po2.tpIntegrador;
 
 public class Categoria {
-    public Categoria(String nombre) {
-        // TODO: Implementar
+
+    private String categoria;
+
+    public Categoria(String categoria) {
+        this.categoria = TextoNormalizado.normalizarTexto(categoria);
+    }
+
+    public String getCategoria() {
+        return TextoNormalizado.normalizarTexto(categoria);
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = TextoNormalizado.normalizarTexto(categoria);
     }
 }
