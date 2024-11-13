94bacb244c8b59b80ccb9a04f93943fdbc12274e
salvanescki <salvanescki.unq@gmail.com>
2024-11-13 04:28:11 -0300

    Agrego Tests de Busqueda

---
+++ src/test/java/ar/edu/unq/po2/tpIntegrador/BusquedaTest.java
@@ -0,0 +1,108 @@
+package ar.edu.unq.po2.tpIntegrador;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class BusquedaTest {
+
+    private List<Publicacion> publicaciones;
+    private LocalDate ahora;
+    private LocalDate enQuinceDias;
+    private Publicacion casaEnQuilmes;
+    private Publicacion dptoEnMdp;
+    private Publicacion quintaEnQuilmes;
+
+    @BeforeEach
+    void setUp() {
+        publicaciones = new ArrayList<>();
+        ahora = LocalDate.now();
+        enQuinceDias = ahora.plusDays(15);
+
+        casaEnQuilmes = mock(Publicacion.class);
+        when(casaEnQuilmes.getCiudad()).thenReturn("Quilmes");
+        dptoEnMdp = mock(Publicacion.class);
+        when(dptoEnMdp.getCiudad()).thenReturn("Mar del Plata");
+        quintaEnQuilmes = mock(Publicacion.class);
+        when(quintaEnQuilmes.getCiudad()).thenReturn("Quilmes");
+
+        when(casaEnQuilmes.getPrecio(ahora, enQuinceDias)).thenReturn(new Precio(10000));
+        when(dptoEnMdp.getPrecio(ahora, enQuinceDias)).thenReturn(new Precio(30000));
+        when(quintaEnQuilmes.getPrecio(ahora, enQuinceDias)).thenReturn(new Precio(20000));
+
+        publicaciones.addAll(Arrays.asList(casaEnQuilmes, dptoEnMdp, quintaEnQuilmes));
+    }
+
+    @Test
+    void busquedaPorCiudadPublicacionesDisponiblesTest() {
+        assertEquals(List.of(casaEnQuilmes, quintaEnQuilmes), new Busqueda("Quilmes", ahora, enQuinceDias).efectuarBusqueda(publicaciones));
+        assertEquals(List.of(dptoEnMdp), new Busqueda("Mar del Plata", ahora, enQuinceDias).efectuarBusqueda(publicaciones));
+        assertTrue(new Busqueda("No soy ciudad valida", ahora, enQuinceDias).efectuarBusqueda(publicaciones).isEmpty());
+        assertTrue(new Busqueda("", ahora, enQuinceDias).efectuarBusqueda(publicaciones).isEmpty());
+    }
+
+    @Test
+    void busquedaPorCiudadPublicacionesYaReservadasEnFechasNoAparecenTest() {
+        when(dptoEnMdp.estaReservadaEnFechas(ahora, enQuinceDias)).thenReturn(true);
+        when(casaEnQuilmes.estaReservadaEnFechas(ahora, enQuinceDias)).thenReturn(true);
+
+        assertTrue(new Busqueda("Mar del Plata", ahora, enQuinceDias).efectuarBusqueda(publicaciones).isEmpty());
+        assertEquals(List.of(quintaEnQuilmes), new Busqueda("Quilmes", ahora, enQuinceDias).efectuarBusqueda(publicaciones));
+    }
+
+    @Test
+    void conPrecioMinimoIgualACeroNoFiltraPublicacionesTest() {
+        assertEquals(new Busqueda("Quilmes", ahora, enQuinceDias).efectuarBusqueda(publicaciones),
+                     new Busqueda("Quilmes", ahora, enQuinceDias).conPrecioMinimo(new Precio(0)).efectuarBusqueda(publicaciones));
+    }
+
+    @Test
+    void conPrecioMinimoFiltraPublicacionesTest() {
+        assertEquals(List.of(quintaEnQuilmes),
+                     new Busqueda("Quilmes", ahora, enQuinceDias).conPrecioMinimo(new Precio(20000)).efectuarBusqueda(publicaciones));
+    }
+
+    @Test
+    void conPrecioMaximoFiltraPublicacionesTest() {
+        assertEquals(List.of(casaEnQuilmes),
+                     new Busqueda("Quilmes", ahora, enQuinceDias).conPrecioMaximo(new Precio(20000)).efectuarBusqueda(publicaciones));
+    }
+
+    @Test
+    void conPrecioMinimoYMaximoIncluyeLosCasosBordeTest() {
+        Precio precioEsperado = new Precio(20000);
+        assertEquals(List.of(dptoEnMdp),
+                     new Busqueda("Mar del Plata", ahora, enQuinceDias)
+                                  .conPrecioMinimo(precioEsperado)
+                                  .conPrecioMaximo(precioEsperado)
+                                  .efectuarBusqueda(publicaciones));
+    }
+
+    @Test
+    void conPrecioMinimoYMaximoFiltranPublicacionesTest() {
+        assertTrue(new Busqueda("Quilmes", ahora, enQuinceDias)
+                        .conPrecioMinimo(new Precio(10001))
+                        .conPrecioMaximo(new Precio(29999))
+                        .efectuarBusqueda(publicaciones)
+                        .isEmpty());
+    }
+
+    @Test
+    void conCantidadDeHuespedesFiltraPublicacionesTest() {
+        when(casaEnQuilmes.getCapacidad()).thenReturn(3);
+        when(quintaEnQuilmes.getCapacidad()).thenReturn(10);
+        assertEquals(List.of(casaEnQuilmes, quintaEnQuilmes),
+                     new Busqueda("Quilmes", ahora, enQuinceDias).conCantidadDeHuespedes(3).efectuarBusqueda(publicaciones));
+        assertEquals(List.of(quintaEnQuilmes),
+                new Busqueda("Quilmes", ahora, enQuinceDias).conCantidadDeHuespedes(5).efectuarBusqueda(publicaciones));
+        assertTrue(new Busqueda("Quilmes", ahora, enQuinceDias).conCantidadDeHuespedes(11).efectuarBusqueda(publicaciones).isEmpty());
+    }
+}
