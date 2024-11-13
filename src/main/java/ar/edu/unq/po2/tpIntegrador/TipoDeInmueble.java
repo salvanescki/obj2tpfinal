f42ea480efb08729a05557bab004fe4fdaf6fbe6
Cristian <cristian.adrian.f.r@gmail.com>
2024-11-12 22:58:36 -0300

    Clase de TipoDeInmueble

--- src/main/java/ar/edu/unq/po2/tpIntegrador/TipoDeInmueble.java
+++ src/main/java/ar/edu/unq/po2/tpIntegrador/TipoDeInmueble.java
@@ -1,4 +1,18 @@
 package ar.edu.unq.po2.tpIntegrador;
 
 public class TipoDeInmueble {
+
+    private String tipoDeInmueble;
+
+    public TipoDeInmueble(String tipoDeInmueble) {
+        this.tipoDeInmueble = tipoDeInmueble;
+    }
+
+    public String getTipoDeInmueble() {
+        return (TextoNormalizado.normalizarTexto(tipoDeInmueble));
+    }
+
+    public void setTipoDeInmueble(String tipoDeInmueble) {
+        this.tipoDeInmueble = TextoNormalizado.normalizarTexto(tipoDeInmueble);
+    }
 }
