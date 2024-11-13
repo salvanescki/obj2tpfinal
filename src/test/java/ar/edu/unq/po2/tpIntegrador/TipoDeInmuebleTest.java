9f6bf0223878617cec0b558ab0761006999c9fce
Cristian <cristian.adrian.f.r@gmail.com>
2024-11-12 22:54:16 -0300

    Test de TipoDeInmueble

---
+++ src/test/java/ar/edu/unq/po2/tpIntegrador/TipoDeInmuebleTest.java
@@ -0,0 +1,29 @@
+package ar.edu.unq.po2.tpIntegrador;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TipoDeInmuebleTest {
+
+    @Test
+    void testConstructor() {
+
+        String nombreTipoDeInmueble = "bungalo";
+
+        TipoDeInmueble tipoDeInmueble = new TipoDeInmueble(nombreTipoDeInmueble);
+
+        assertEquals(TextoNormalizado.normalizarTexto(nombreTipoDeInmueble), tipoDeInmueble.getTipoDeInmueble());
+    }
+
+    @Test
+    void testSetTipoDeInmueble() {
+
+        TipoDeInmueble tipoDeInmueble = new TipoDeInmueble("Bungalo");
+        String nuevoTipoDeInmueble = "Apartamento";
+
+        tipoDeInmueble.setTipoDeInmueble(nuevoTipoDeInmueble);
+
+        assertEquals(TextoNormalizado.normalizarTexto(nuevoTipoDeInmueble), tipoDeInmueble.getTipoDeInmueble());
+    }
+}
