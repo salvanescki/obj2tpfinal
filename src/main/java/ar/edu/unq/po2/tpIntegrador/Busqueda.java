f060677653e3c3abe301c62e317630e61d545eaa
salvanescki <salvanescki.unq@gmail.com>
2024-11-13 04:51:47 -0300

    Termino implementación de Busqueda

--- src/main/java/ar/edu/unq/po2/tpIntegrador/Busqueda.java
+++ src/main/java/ar/edu/unq/po2/tpIntegrador/Busqueda.java
@@ -1,30 +1,48 @@
 package ar.edu.unq.po2.tpIntegrador;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class Busqueda {
-    public Busqueda(String ciudad, LocalDate fechaEntrada, LocalDate fechaSalida) {
-        // TODO: Implementar
-    }
 
-    public List<Publicacion> efectuarBusqueda(List<Publicacion> publicaciones) {
-        // TODO: Implementar
-        return List.of();
+    private String ciudad;
+    private LocalDate fechaEntrada;
+    private LocalDate fechaSalida;
+    private List<Predicate<Publicacion>> filtrosOpcionales;
+
+    public Busqueda(String ciudad, LocalDate fechaEntrada, LocalDate fechaSalida) {
+        this.ciudad = ciudad;
+        this.fechaEntrada = fechaEntrada;
+        this.fechaSalida = fechaSalida;
+        filtrosOpcionales = new ArrayList<>();
     }
 
     public Busqueda conPrecioMinimo(Precio precio) {
-        // TODO: Implementar
+        filtrosOpcionales.add(p -> precio.getPrecio() <= p.getPrecio(fechaEntrada, fechaSalida).getPrecio());
         return this;
     }
 
     public Busqueda conPrecioMaximo(Precio precio) {
-        // TODO: Implementar
+        filtrosOpcionales.add(p -> precio.getPrecio() >= p.getPrecio(fechaEntrada, fechaSalida).getPrecio());
         return this;
     }
 
     public Busqueda conCantidadDeHuespedes(int cantidad) {
-        // TODO: Implementar
+        filtrosOpcionales.add(p -> p.getCapacidad() >= cantidad);
         return this;
+    }
+
+    public List<Publicacion> efectuarBusqueda(List<Publicacion> publicaciones) {
+        Stream<Publicacion> listaFiltrada = publicaciones.stream()
+                                                        .filter(p -> p.getCiudad().equals(ciudad))
+                                                        .filter(p -> !p.estaReservadaEnFechas(fechaEntrada, fechaSalida));
+        for(Predicate<Publicacion> filtro : filtrosOpcionales){
+            listaFiltrada = listaFiltrada.filter(filtro);
+        }
+
+        return listaFiltrada.toList();
     }
 }
