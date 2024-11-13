c396c5846cfc444e7638b6f9cca24d7a9ca03b29
salvanescki <salvanescki.unq@gmail.com>
2024-11-13 19:42:12 -0300

    Refactor Servicio, Categoria, TipoDeInmueble y FormaDePago

--- src/main/java/ar/edu/unq/po2/tpIntegrador/Servicio.java
+++ src/main/java/ar/edu/unq/po2/tpIntegrador/Servicio.java
@@ -1,17 +1,26 @@
 package ar.edu.unq.po2.tpIntegrador;
 
 public class Servicio {
+
     private String servicio;
 
     public Servicio(String servicio) {
-        this.servicio = servicio;
+        setServicio(servicio);
     }
 
     public String getServicio() {
-        return (TextoNormalizado.normalizarTexto(servicio));
+        return servicio;
     }
 
     public void setServicio(String servicio) {
         this.servicio = TextoNormalizado.normalizarTexto(servicio);
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(this.getClass() != o.getClass()) return false;
+        Servicio otro = (Servicio) o;
+        return this.servicio.equals(otro.getServicio());
     }
 }
