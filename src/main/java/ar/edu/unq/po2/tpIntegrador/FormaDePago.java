24f9cff8f8a450e0213d80e94a9a8a9268f56522
Cristian <cristian.adrian.f.r@gmail.com>
2024-11-13 10:23:21 -0300

    Se realizo la clase FormaDePago

--- src/main/java/ar/edu/unq/po2/tpIntegrador/FormaDePago.java
+++ src/main/java/ar/edu/unq/po2/tpIntegrador/FormaDePago.java
@@ -1,4 +1,17 @@
 package ar.edu.unq.po2.tpIntegrador;
 
 public class FormaDePago {
+    private String formaDePago;
+
+    public FormaDePago(String formaDePago) {
+        this.formaDePago = formaDePago;
+    }
+
+    public String getFormaDePago() {
+        return TextoNormalizado.normalizarTexto(formaDePago);
+    }
+
+    public void setFormaDePago(String formaDePago) {
+        this.formaDePago = TextoNormalizado.normalizarTexto(formaDePago);
+    }
 }
